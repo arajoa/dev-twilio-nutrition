bb1016331c56fbd7882dd700bf95b7fa603292c8
Rafael Vindel Amor <ramor@homeaway.com>
2020-04-10 15:22:18 +0200

    Refactor to use IOApp

--- src/main/scala/io/arajoa/hackathon/Main.scala
+++ src/main/scala/io/arajoa/hackathon/Main.scala
@@ -1,11 +1,26 @@
 package io.arajoa.hackathon
 
-import cats.effect.IO
-import com.twitter.finagle.Http
-import com.twitter.util.Await
+import cats.effect.{ExitCode, IO, IOApp, Resource}
+import cats.implicits._
+import com.twitter.finagle.{Http, ListeningServer}
+import io.arajoa.hackathon.ConversionDataTypes._
 import io.finch._
 
-object Main extends App with Endpoint.Module[IO] {
+object Main extends IOApp with Endpoint.Module[IO] {
   val api: Endpoint[IO, String] = get("hello") { Ok("Hello, World!") }
-  Await.ready(Http.server.serve(":8080", api.toServiceAs[Text.Plain]))
+
+  def server: Resource[IO, ListeningServer] =
+    Resource.make {
+      IO(Http.server.serve(":8080", api.toServiceAs[Text.Plain]))
+    } { s => IO.fromFuture(IO(s.close().asScala)) }
+
+  override def run(args: List[String]): IO[ExitCode] =
+    for {
+      _ <- server
+        .use(_ => IO(println("app started")) *> IO.never)
+        .handleErrorWith(error => {
+          IO(println("error starting")) *>
+            IO.raiseError(error)
+        })
+    } yield ExitCode.Success
 }
