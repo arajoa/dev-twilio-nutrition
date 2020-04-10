e1edbdf7d3b9466fd8171296a6a09bda2cea1828
Jose Luis Rodriguez <jlr.villapecellin@gmail.com>
2020-04-10 15:28:09 +0200

    Fix format

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,9 +3,7 @@ import sbt._
 object Version {
 
   val finchx       = "0.32.1"
-  val finchxCirce  = "0.32.1"
   val circe        = "0.13.0"
-  val circeGeneric = "0.13.0"
   val cats         = "2.1.1"
   val catsEffect   = "2.1.1"
   val scalaTest    = "3.1.1"
@@ -14,9 +12,9 @@ object Version {
 object Library {
 
   val finchx       = "com.github.finagle" %% "finchx-core"   % Version.finchx
-  val finchXCirce  = "com.github.finagle" %% "finchx-circe"  % Version.finchxCirce
+  val finchXCirce  = "com.github.finagle" %% "finchx-circe"  % Version.finchx
   val circe        = "io.circe"           %% "circe-core"    % Version.circe
-  val circeGeneric = "io.circe"           %% "circe-generic" % Version.circeGeneric
+  val circeGeneric = "io.circe"           %% "circe-generic" % Version.circe
   val cats         = "org.typelevel"      %% "cats-core"     % Version.cats
   val catsEffect   = "org.typelevel"      %% "cats-effect"   % Version.catsEffect
 
