f0cef5e3af03592df19e3460a2270464f01d92f9
Rafael Vindel Amor <rafael.vindel@gmail.com>
2020-04-10 16:26:49 +0200

    [Refactor] Use IOApp

---
+++ src/main/scala/io/arajoa/hackathon/ConversionDataTypes.scala
@@ -0,0 +1,34 @@
+package io.arajoa.hackathon
+
+import com.twitter.util.{Return, Throw, Future => TwitterFuture, Promise => TwitterPromise}
+
+import scala.concurrent.{ExecutionContext, Future => ScalaFuture, Promise => ScalaPromise}
+import scala.util.{Failure, Success}
+
+object ConversionDataTypes {
+
+  /** Convert from a Twitter Future to a Scala Future */
+  implicit class RichTwitterFuture[A](val tf: TwitterFuture[A]) extends AnyVal {
+    def asScala: ScalaFuture[A] = {
+      val promise: ScalaPromise[A] = ScalaPromise()
+      tf.respond {
+        case Return(value)    => promise.success(value)
+        case Throw(exception) => promise.failure(exception)
+      }
+      promise.future
+    }
+  }
+
+  /** Convert from a Scala Future to a Twitter Future */
+  implicit class RichScalaFuture[A](val sf: ScalaFuture[A]) extends AnyVal {
+    def asTwitter(implicit e: ExecutionContext): TwitterFuture[A] = {
+      val promise: TwitterPromise[A] = new TwitterPromise[A]()
+      sf.onComplete {
+        case Success(value)     => promise.setValue(value)
+        case Failure(exception) => promise.setException(exception)
+      }
+      promise
+    }
+  }
+
+}
